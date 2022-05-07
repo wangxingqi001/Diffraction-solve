8ae0d256038dc3898bc410f4f984f0bdf553f116
wangxingqi001 <34742743+wangxingqi001@users.noreply.github.com>
2022-05-08 00:08:30 +0800

    Add files via upload

---
+++ ui/compile_ui.sh
@@ -0,0 +1,9 @@
+pyuic4 param_setting.ui -o ../src/pygui/param_setting_gui.py
+pyuic4 main.ui -o ../src/pygui/main_gui.py
+pyuic4 start.ui -o ../src/pygui/start_gui.py
+pyuic4 run.ui -o ../src/pygui/run_gui.py
+pyuic4 chosebox.ui -o ../src/pygui/chosebox_gui.py
+pyuic4 jobView.ui -o ../src/pygui/jobview_gui.py
+pyuic4 job_benchmark.ui -o ../src/pygui/benchmark_gui.py
+
+pyuic4 data_viewer.ui -o ../src/data_viewer/layout.py
